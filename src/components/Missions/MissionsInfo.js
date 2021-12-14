05a6f62311951718839c2843d7647e147e925520
VanessaAoki <66967274+VanessaAoki@users.noreply.github.com>
2021-12-14 18:05:59 -0300

    Update missions info to have buttons join and leave

--- src/components/Missions/MissionsInfo.js
+++ src/components/Missions/MissionsInfo.js
@@ -1,45 +1,68 @@
-/* eslint-disable react/jsx-key */
-import { React, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { loadMissionsData } from '../../redux/Missions/missions';
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { joinMissions, leaveMissions } from '../../redux/Missions/missions';
 
-const MissionsInfo = () => {
-  const missions = useSelector((state) => state.missions);
+const MissionsInfo = (props) => {
+  const { data } = props;
+  const missionName = data.mission_name;
+  const id = data.mission_id;
+  const { description, status } = data;
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    if (missions.length === 0) {
-      dispatch(loadMissionsData());
+  const joinMissionsButton = () => {
+    dispatch(joinMissions(id));
+  };
+
+  const leaveMissionsButton = () => {
+    dispatch(leaveMissions(id));
+  };
+
+  const statusChange = () => {
+    if (status === true) {
+      leaveMissionsButton();
+    } else {
+      joinMissionsButton();
     }
-  }, []);
+  };
 
   return (
-    <>
-    {missions.map((mission) => (
-      <tr>
-        <td>
-          <b>{mission.mission_name}</b>
-        </td>
-        <td>
-          {mission.description}
-        </td>
-        <td className="button-container">
-          <button type="button" className="button-status">
-            <span>
-              NOT A MEMBER
-            </span>
-          </button>
-        </td>
-        <td className="button-container">
-          <button type="button" className="button-join">
-            <span>
-              Join Mission
-            </span>
-          </button>
-        </td>
-      </tr>
-    ))}
-    </>
+    <tr>
+      <td>
+        <b id="missionName">{missionName}</b>
+      </td>
+      <td id="missionDescription">
+        {description}
+      </td>
+      <td className="button-container">
+        <button
+          type="button"
+          className={status
+            ? 'button-status button-status-active'
+            : 'button-status'}
+        >
+          <span>
+            {status
+              ? 'Active Member'
+              : 'NOT A MEMBER'}
+          </span>
+        </button>
+      </td>
+      <td className="button-container">
+        <button
+          type="button"
+          className={status
+            ? 'button-join button-join-active'
+            : 'button-join'}
+          onClick={statusChange}
+        >
+          <span>
+            {status
+              ? 'Cancel Mission'
+              : 'Join Mission'}
+          </span>
+        </button>
+      </td>
+    </tr>
   );
 };
 
