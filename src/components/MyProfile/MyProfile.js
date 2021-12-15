81103eb04c7cd070093d1ef7edace6abaff692dc
VanessaAoki <66967274+VanessaAoki@users.noreply.github.com>
2021-12-14 21:28:17 -0300

    Add my profile file structure

--- src/components/MyProfile/MyProfile.js
+++ src/components/MyProfile/MyProfile.js
@@ -1,7 +1,18 @@
 import React from 'react';
+import MissionFilter from './MissionFilter';
+import RocketsFilter from './RocketsFilter';
 
 const MyProfile = () => (
-  <h2>My Profile</h2>
+  <div>
+    <section>
+      <h2>Missions</h2>
+      <ul><MissionFilter /></ul>
+    </section>
+    <section>
+      <h2>Missions</h2>
+      <ul><RocketsFilter /></ul>
+    </section>
+  </div>
 );
 
 export default MyProfile;
